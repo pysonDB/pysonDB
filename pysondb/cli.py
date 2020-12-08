a554c4bd8ea6401c0d14c019dd536cbc90a13e3c
Sean Beck <seanmckaybeck@gmail.com>
2020-12-08 08:42:19 -0500

    Update cli.py
    * Organize your imports
    * pep8 spacing
    * Use type hints for your function parameters
    * Write docstrings for your code (gave one example)
    * Use descriptive variable names instead of `x` or `y`: IDEs have tab completion for a reason

--- pysondb/cli.py
+++ pysondb/cli.py
@@ -1,87 +1,71 @@
-import fire
-from pathlib import Path
-import json
-from beautifultable import BeautifulTable
-import os
 import csv
+import json
+import os
 import random
-def create(name):
+
+from beautifultable import BeautifulTable
+import fire
+
+
+def create_if_not_exist(file_name: str):
     """ 
-    The below function checks if there is a similar .json file
-    If the file exists It does nothing and it will add the filename variable to the class. 
-    But if the file is not present it will add {data:[]} and will add the filename variable to the class.
+    Checks for the existence of the provided JSON DB.
+    If it does not, this will add {data:[]}.
+    :param str file_name: The absolute path to the DB file
     """
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as db_file:
+            db = {'data': []}
+            json.dump(db, db_file)
+        print("Succesfully created {} in the directory.".format(name))
 
-    sdx=Path(name)
-    if sdx.exists():
-        x=open(name,'r+')
-        yt=x.read()
-        try:
-            cd=json.loads(yt)
-            d=cd['data']
-            x.close()
-            return("File exists")
-            
-        except:
-            xy={'data':[]}
-            x.write(json.dumps(xy))
-            x.close()
-
-    else:
-        xy={'data':[]}
-        y=open(name,'a')
-        y.write(json.dumps(xy))
-        y.close()
-        return("Succesfull created {} in the directory.".format(name))
-def display(name):
+    
+def display(file_name: str):
     table = BeautifulTable()
- 
-    with open(name,'r+') as jsondoc:
-        
-        data=json.load(jsondoc)
-        real_data=data['data']
-        header=list(data["data"][0].keys())
+    with open(file_name) as jsondoc:
+        data = json.load(jsondoc)
+        real_data = data['data']
+        header = list(data["data"][0].keys())
         for all_data in real_data:
             table.rows.append(list(all_data.values()))
-        table.columns.header=header
-
+        table.columns.header = header
         print(table)
-def delete(name):
-    if os.path.exists(name):
-        x=input("Do you want to remove the json file..(y/n)")
-        if x=="y" or x=="Y":
+
+
+def delete(file_name: str):
+    if os.path.exists(file_name):
+        x = input("Do you want to remove the json file..(y/n)")
+        if x in ['y', 'Y']:
             os.remove(name)
         else:
             print("Action terminated")    
     else:
         print("The file does not exist")
-def convert(c,d):
-    print("Reading data from {}".format(c))
-    arr=[]
-    with open (c) as csvFile:
+
+
+def convert(csv_file, json_db):
+    print("Reading data from {}".format(csv_file))
+    arr = []
+    with open(csv_file) as csvFile:
         csvReader = csv.DictReader(csvFile)
         for csvRow in csvReader:
-            csvRow['id']=random.randint(1000000000,9999999999)
+            csvRow['id'] = random.randint(1000000000,9999999999)
             arr.append(csvRow)
+    print("Writing data into {}".format(json_db))
+    x = {}
+    x["data"] = arr
+    with open(json_db, 'w') as json_file:
+        json.dump(x, json_file)
+    print("Conversion succesful")
 
-
-    print("Writing data into {}".format(d))
-    x={}
-    x["data"]=arr
-    y=json.dumps(x)
-    filetowrite=open(d,"w")
-    filetowrite.write(y)
-    filetowrite.close()
-    print("Conversion Succesfull")
 
 def main():
     fire.Fire({
-        'create':create,
+        'create':create_if_not_exist,
         'display':display,
         'delete':delete,
         'convert':convert
     })
-
 
 
 if __name__ == '__main__':
