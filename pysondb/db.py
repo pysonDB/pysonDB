6b2a27a6934e2a4fa421ff448ad4d70cf1926213
Fredy Somy <fredysomy@gmail.com>
2021-01-17 00:01:38 +0530

    Update db.py

--- pysondb/db.py
+++ pysondb/db.py
@@ -13,16 +13,11 @@ class getDb:
         # If the file exists It does nothing and it will add the filename variable to the class. 
         # But if the file is not present it will add {data:[]} and will add the filename variable to the class.
         print(sdx)
-        try:
-            x=open(filename,'r+')
-            yt=x.read()
-            self.filename=filename
-        except:
-            xy={'data':[]}
-            y=open(filename,'a')
-            y.write(json.dumps(xy))
-            y.close()
-            self.filename=filename
+        
+        x=open(filename,'r+',encoding="utf-8")
+        yt=x.read()
+        self.filename=filename
+       
     def add(self,data2):
         class SchemmaNotMatchError(Exception):
             pass
