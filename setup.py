a81a2dd16d5d0db32056dad682a1735c0c529f4a
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-29 16:53:58 +0530

    bumped the version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     license="MIT License",
     long_description=readme,
     long_description_content_type="text/markdown",
-    version="1.1.1",
+    version="1.1.2",
     keywords="pysondb,database,json,yaml",
     name="pysondb",
     packages=["pysondb"],
