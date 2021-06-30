4c7c46ca310b0f7a149cf73383a120e5ed4d9469
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-06-30 20:07:00 +0530

    Updated error messages and type hint error class

--- pysondb/db.py
+++ pysondb/db.py
@@ -5,8 +5,8 @@ import re
 import uuid
 from pathlib import Path
 from types import SimpleNamespace
+from typing import Any, Dict
 
-import yaml
 from filelock import FileLock
 
 # consants
@@ -24,8 +24,11 @@ class DataNotFoundError(Exception):
         data
     """
 
-    def __init__(self, data):
+    def __init__(self, data: Dict[str, Any]) -> None:
         self.data = data
+
+    def __str__(self) -> str:
+        return f"The data {self.data!r} does not exists in JSON db"
 
 
 class IdNotFoundError(Exception):
@@ -35,15 +38,21 @@ class IdNotFoundError(Exception):
         pk -- primary key / id
     """
 
-    def __init__(self, pk):
+    def __init__(self, pk: int) -> None:
         self.pk = pk
+
+    def __str__(self) -> str:
+        return f"Id {self.pk!r} does not exist in the JSON db"
 
 
 class SchemaError(Exception):
     """Exception raised for field/key errors."""
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         self.args = args
+
+    def __str__(self) -> str:
+        return str(self.args)
 
 
 # util functions
@@ -108,7 +117,9 @@ class JsonDatabase:
                         [self.id_fieldname]
                     ):
                         new_data[self.id_fieldname] = self._get_id()
+
                         logger.debug("Append new data; {0}".format(new_data))
+
                         db_data["data"].append(new_data)
                         db_file.seek(0)
                         self._get_dump_function()(db_data, db_file)
--- pysondb/imageutils/imageutils.py
+++ pysondb/imageutils/imageutils.py
@@ -2,52 +2,70 @@ from pysondb import db
 import os
 import base64
 
+
 class PathNotFoundError(Exception):
     """Exception raised for non-existent image path."""
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         self.args = args
+
+    def __str__(self) -> str:
+        return str(self.args)
+
 
 class NoNameError(Exception):
     """Exception raised when no name is passed as params along with the add_image function """
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         self.args = args
+
+    def __str__(self) -> str:
+        return str(self.args)
+
+
 class SaveError(Exception):
     """Exception raised when there is a error in saving the image"""
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         self.args = args
 
+    def __str__(self) -> str:
+        return str(self.args)
+
+
 class setdb:
-    def __init__(self,db_name):
-        self.db_name=db_name
-        self.db=db.getDb(db_name)
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.db = db.getDb(db_name)
 
-
-    def add_image(self,src,name):
-        if(os.path.exists(src)):
-           with open(src,"rb") as img_data:
-                raw_data=base64.b64encode(img_data.read())
-                if(name):
-                    name_list=src.split("/")
-                    self.db.add({"data":raw_data.decode('utf-8'),"name":name,"fname":name_list[len(name_list)-1]})
+    def add_image(self, src, name):
+        if os.path.exists(src):
+            with open(src, "rb") as img_data:
+                raw_data = base64.b64encode(img_data.read())
+                if name:
+                    name_list = src.split("/")
+                    self.db.add(
+                        {
+                            "data": raw_data.decode("utf-8"),
+                            "name": name,
+                            "fname": name_list[len(name_list) - 1],
+                        }
+                    )
                     print(f"The image is stored in the database :{self.db_name}")
                 else:
                     raise NoNameError("No name was passed with the params")
         else:
             raise PathNotFoundError("The src image could not be found")
 
-
-    def get_image(self,name):
-        if(name):
+    def get_image(self, name):
+        if name:
             try:
-                img_data=self.db.getBy({"name":name})
-                f=open(img_data[0]["fname"],"wb")
-                f.write(base64.decodebytes(bytes(img_data[0]['data'],"utf-8")))
+                img_data = self.db.getBy({"name": name})
+                f = open(img_data[0]["fname"], "wb")
+                f.write(base64.decodebytes(bytes(img_data[0]["data"], "utf-8")))
                 f.close()
             except Exception as e:
                 raise SaveError(e)
 
         else:
-            raise NoNameError("No name found")    
+            raise NoNameError("No name found")
