1d2568e581f8a69970be4a7a2c2afb35e4a8e12b
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-07-04 17:05:46 +0530

    Added a method to delete all the records from the DB

--- pysondb/db.py
+++ pysondb/db.py
@@ -288,6 +288,11 @@ class JsonDatabase:
                 self._get_dump_function()(db_data, db_file)
             return True
 
+    def deletAll(self) -> None:
+        with self.lock:
+            with open(self.filename, "w") as f:
+                f.write(json.dumps(EMPTY_DATA))
+
     def update(self, db_dataset: Dict[str, Any], new_dataset: Dict[str, Any]) -> None:
         with self.lock:
             with open(self.filename, "r+") as db_file:
